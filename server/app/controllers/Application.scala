086e732492bb5ae9d650777e4b2bf3587bbb7196
Technius <techniux@gmail.com>
2015-02-14 09:32:38 -0800

    Cache webpage requests

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -1,11 +1,15 @@
 package controllers
 
 import play.api._
+import play.api.cache.Cached
 import play.api.mvc._
+import play.api.Play.current
 
 object Application extends Controller {
   val scriptPath = if (Play.isProd(Play.current)) "app.js" else "app-dev.js"
-  def serve(ignored: String = "") = Action {
-    Ok(views.html.index(scriptPath))
+  def serve(ignored: String = "") = Cached("webpage") {
+    Action {
+      Ok(views.html.index(scriptPath))
+    }
   }
 }
