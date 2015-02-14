ad732516de9f8d8db549594e1ca19cebeab62b13
Technius <techniux@gmail.com>
2015-02-13 19:38:13 -0800

    Fix page serving

--- server/app/controllers/Application.scala
+++ server/app/controllers/Application.scala
@@ -5,7 +5,7 @@ import play.api.mvc._
 
 object Application extends Controller {
   val scriptPath = if (Play.isProd(Play.current)) "app.js" else "app-dev.js"
-  def serve = Action {
+  def serve(ignored: String = "") = Action {
     Ok(views.html.index(scriptPath))
   }
 }
